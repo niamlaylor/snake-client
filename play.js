152817584c00ce9ab7daee7e5ab446fbbe4f33bd
Liam Naylor <liam@liamnaylor.com>
2022-10-17 19:07:11 -0700

    Add client.js and exports from play.js

--- play.js
+++ play.js
@@ -1,19 +1,5 @@
 const net = require('net');
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '192.168.0.23',
-    port: '50541'
-  });
-  // this event handler console logs data from the server
-  conn.on('data', (data) => {
-    console.log('Incoming message: ', data);
-  })
-  //interpret incoming data as text
-  conn.setEncoding('utf8');
-  return conn;
-};
+const connect = require('./client');
 
 console.log('Connecting ...');
 connect();

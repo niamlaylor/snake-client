2e50e2880724495a901bf8cd716be9efe39e3bfc
Liam Naylor <liam@liamnaylor.com>
2022-10-17 19:34:15 -0700

    Add functionality to send name to game server

--- client.js
+++ client.js
@@ -6,10 +6,18 @@ const connect = function () {
     host: '192.168.0.23',
     port: '50541'
   });
+
   // this event handler console logs data from the server
   conn.on('data', (data) => {
     console.log('Incoming message: ', data);
-  })
+  });
+
+  // this event handler prints a confirmation as soon as we conect to the game server
+  conn.on('connect', () => {
+    console.log('Successfully connected to the game server!');
+    conn.write('Name: LNN')
+  });
+
   // interpret incoming data as text
   conn.setEncoding('utf8');
   return conn;
